0ce4baee6012199f750fb131dc1d99a509e4c58a
juniar <juniar.rahman@rithm.nl>
2020-07-26 11:58:22 +0700

    added more stuff

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>
+  <div class="col-full">
+    <h1>Welcome to Vue Forum</h1>
+    <ThreadList :threads="threads"/>
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
+  import ThreadList from '@/components/ThreadList'
+
+  console.log(sourceData)
 
   export default {
     name: 'HelloWorld',
+    components: {ThreadList},
     data () {
       return {
-        threads: sourceData.threads,
+        threads: Object.values(sourceData.threads),
         posts: sourceData.posts,
         users: sourceData.users
       }
---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="thread">
+    <div>
+      <p><a href="#">{{thread.title}}</a></p>
+      <p class="text-faded text-xsmall"> By <a href="#">{{user}}</a>{{thread.publishedAt}}</p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        {{repliesCount}}
+      </p>
+
+      <!--      <img class="avatar-medium"-->
+      <!--           src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png" alt="">-->
+
+      <!--      <div>-->
+      <!--        <p class="text-xsmall">-->
+      <!--          <a href="#">Bruce Wayne</a>-->
+      <!--        </p>-->
+      <!--        <p class="text-xsmall text-faded">2 hours ago</p>-->
+      <!--      </div>-->
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data'
+
+  export default {
+    name: 'ThreadListItem',
+    props: {
+      thread: {
+        required: true,
+        type: Object
+      }
+    },
+    computed: {
+      repliesCount () {
+        let counter = Object.keys(this.thread.posts).length - 1
+        let suffix = counter > 1 ? ' replies' : ' reply'
+        return counter + suffix
+      },
+      user () {
+        return sourceData.users[this.thread.userId].name
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
